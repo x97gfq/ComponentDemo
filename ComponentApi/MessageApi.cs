30b1dbdeb12f9c98212a5c9d05ca5323c58b28e6
Jamie Symonds <x97gfq@hotmail.com>
2020-02-08 16:56:14 -0400

    Add project files.

---
+++ ComponentApi/MessageApi.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Net.Sockets;
+
+namespace ComponentApi
+{
+    public class MessageApi
+    {
+        public string Greet()
+        {
+            string greeting = GetDateTimeMessageFromService();
+            return greeting;
+        }
+
+        private string GetDateTimeMessageFromService()
+        {
+            var client = new TcpClient("time.nist.gov", 13);
+            var localDateTimeString = string.Empty;
+            using (var streamReader = new StreamReader(client.GetStream()))
+            {
+                var response = streamReader.ReadToEnd();
+                var utcDateTimeString = response.Substring(7, 17);
+                var localDateTime = DateTime.ParseExact(utcDateTimeString, "yy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture,DateTimeStyles.AssumeUniversal);
+                localDateTimeString = (localDateTime.ToLongDateString() + " " + localDateTime.ToLongTimeString());
+            }
+            return localDateTimeString;
+        }
+    }
+}
---
+++ ComponentDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ComponentApi;
+
+namespace ComponentDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MessageApi api = new MessageApi();
+
+            var greeting = api.Greet();
+
+            Console.WriteLine(greeting);
+
+            Console.ReadKey();
+        }
+    }
+}
